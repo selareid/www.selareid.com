9342ae75f241350b900ddc3c794c3933c8a147ee
Daniel <daniel@selareid.com>
2022-02-27 01:02:17 +1300

    Project component is now a takes name,description,image in props

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import {Box, Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
 import "./Projects.css";
 
-const Project: React.FC = () => {
+interface ProjectProp {
+    name: string,
+    description: string,
+    image: string,
+}
+
+const Project: React.FC<{ project: ProjectProp }> = ({project}) => {
     return (
         <Grid item xs={1}>
             <Card color="secondary">
@@ -11,17 +17,12 @@ const Project: React.FC = () => {
                         <CardMedia
                             component="img"
                             sx={{width: 125}}
-                            image="https://birddwnload.weebly.com/uploads/1/2/4/8/124814239/528431018.jpg"
+                            image={project.image}
                             alt="Project Image"
                         />
                         <Box>
-                            <Typography variant="h5">
-                                Project Name
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
+                            <Typography variant="h5">{project.name}</Typography>
+                            <Typography variant="body2" color="text.secondary">{project.description}</Typography>
                         </Box>
                     </CardContent>
                 </CardActionArea>
@@ -35,7 +36,6 @@ const Projects: React.FC = () => {
     return (
         <Container fixed className="contentContainer">
             <Grid container spacing={1} columns={1}>
-                {[<Project/>,<Project/>,<Project/>,<Project/>,<Project/>,<Project/>,<Project/>]}
             </Grid>
         </Container>
     )
