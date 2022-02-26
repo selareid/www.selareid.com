8e73a1987138f955297269af650067d9fb4d1033
Daniel <daniel@selareid.com>
2022-02-26 16:37:36 +1300

    Created Projects page files with basic skeleton

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Container} from "@mui/material";
+import "./Projects.css";
+
+const Projects: React.FC = () => {
+    return (
+        <Container fixed className="contentContainer">
+            {/* -- Project Content Goes Here -- */}
+        </Container>
+    )
+};
+
+export default Projects;
