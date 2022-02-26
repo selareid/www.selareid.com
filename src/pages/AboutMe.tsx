46e18c9017b80c661b3c21dcac7f0d5ec91f6e15
Daniel <daniel@selareid.com>
2022-02-26 15:51:54 +1300

    Edited AboutMe Content

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,15 +1,17 @@
-import {Container} from "@mui/material";
 import React from "react";
+import {Button, Container} from "@mui/material";
 import "./AboutMe.css";
 
 const AboutMe: React.FC = () => {
     return (
         <Container fixed className="contentContainer">
             <h1>UwU 👉👈</h1>
+            <p>Curious about the world and how things work</p>
             <p>
-                I'm a student at the University of Auckland <br/>
-                Majoring in Mathematics and Computer Science <br/>
-                Curious about the world and how/why things work <br/>
+                I'm a student at the University of Auckland<br/>
+                Majoring in Mathematics and Computer Science
+            </p>
+            <p>
                 Current Interests: Rust, Maths, React <br/>
                 Languages I've Used: Rust, JS/TX, HTML/CSS, Python, Java
             </p>
