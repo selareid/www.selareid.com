1050b132c25061a651bd11e8df7b5a679bb708db
Daniel <daniel@selareid.com>
2022-02-27 00:15:04 +1300

    Added grid and populated with some place holder project cards

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,37 @@
 import React from "react";
-import {Container} from "@mui/material";
+import {Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
 import "./Projects.css";
 
 const Projects: React.FC = () => {
+    const c =(
+    <Grid item xs={1}>
+        <Card color="secondary">
+            <CardActionArea sx={{display: "flex", alignItems: "flex-start"}}>
+                <CardMedia
+                    component="img"
+                    sx={{width: 125}}
+                    image="https://birddwnload.weebly.com/uploads/1/2/4/8/124814239/528431018.jpg"
+                    alt="Project Image"
+                />
+                <CardContent sx={{m: 2, p: 0}}>
+                    <Typography gutterBottom variant="h4">
+                        Project Name
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    </Grid>);
+
+
     return (
         <Container fixed className="contentContainer">
-            {/* -- Project Content Goes Here -- */}
+            <Grid container spacing={1} columns={1}>
+                {[c, c, c, c]}
+            </Grid>
         </Container>
     )
 };
