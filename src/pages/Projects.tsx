a43395fd6a54b4b7e79833d2db625683f2a19150
Daniel <daniel@selareid.com>
2022-02-27 00:55:19 +1300

    Moved Project to it's own component, Added Some Styling in Projects.css

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,36 +1,41 @@
 import React from "react";
-import {Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
+import {Box, Card, CardActionArea, CardContent, CardMedia, Container, Grid, Typography} from "@mui/material";
 import "./Projects.css";
 
+const Project: React.FC = () => {
+    return (
+        <Grid item xs={1}>
+            <Card color="secondary">
+                <CardActionArea>
+                    <CardContent className="projectCards">
+                        <CardMedia
+                            component="img"
+                            sx={{width: 125}}
+                            image="https://birddwnload.weebly.com/uploads/1/2/4/8/124814239/528431018.jpg"
+                            alt="Project Image"
+                        />
+                        <Box>
+                            <Typography variant="h5">
+                                Project Name
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                            </Typography>
+                        </Box>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
+    );
+}
+
+
 const Projects: React.FC = () => {
-    const c =(
-    <Grid item xs={1}>
-        <Card color="secondary">
-            <CardActionArea sx={{display: "flex", alignItems: "flex-start"}}>
-                <CardMedia
-                    component="img"
-                    sx={{width: 125}}
-                    image="https://birddwnload.weebly.com/uploads/1/2/4/8/124814239/528431018.jpg"
-                    alt="Project Image"
-                />
-                <CardContent sx={{m: 2, p: 0}}>
-                    <Typography gutterBottom variant="h4">
-                        Project Name
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    </Grid>);
-
-
     return (
         <Container fixed className="contentContainer">
             <Grid container spacing={1} columns={1}>
-                {[c, c, c, c]}
+                {[<Project/>,<Project/>,<Project/>,<Project/>,<Project/>,<Project/>,<Project/>]}
             </Grid>
         </Container>
     )
