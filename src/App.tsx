12ae2916ea34cc8d756ac1c2bc4ee9020b368e0f
Daniel <daniel@selareid.com>
2022-02-26 16:39:34 +1300

    Added route to load project page Projects button now links to /projects

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Button, ButtonGroup} from "@mui/material";
 import './pages/AboutMe';
 import AboutMe from "./pages/AboutMe";
+import Projects from "./pages/Projects";
 import {BrowserRouter, Link, Routes, Route, Navigate} from "react-router-dom";
 
 function App() {
@@ -12,12 +13,13 @@ function App() {
                 <header className="App-header">
                     <ButtonGroup variant="text" color="secondary" >
                         <Button className="navButton" component={Link} to={"/"}>home / about me</Button>
-                        <Button className="navButton" component={Link} to={"/"}>Projects</Button>
+                        <Button className="navButton" component={Link} to={"/projects"}>Projects</Button>
                     </ButtonGroup>
                 </header>
 
                 <Routes>
                     <Route index element={<AboutMe/>}/>
+                    <Route path="projects" element={<Projects/>}/>
                     <Route path="*" element={<Navigate to="/"/>}/>
                 </Routes>
             </div>
