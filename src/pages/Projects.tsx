ae91f9b95c82527ff3297ba706d28e8125d0cd82
Daniel <daniel@selareid.com>
2022-03-04 14:30:16 +1300

    moved the component Project from the Projects.tsx page file to components/Project.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,56 +1,6 @@
-import React, {useState} from "react";
-import {Box, Card, CardActionArea, CardContent, CardMedia, Container, Grid, Modal, Typography} from "@mui/material";
+import {Container, Grid} from "@mui/material";
 import "./Projects.css";
-
-interface ProjectProp {
-    name: string,
-    description: string,
-    image: string,
-    expandedDesc?: JSX.Element
-}
-
-const Project: React.FC<{ project: ProjectProp }> = ({project}) => {
-    const [open, setOpen] = useState(false);
-
-    const card = <Card color="secondary">
-        <CardActionArea onClick={() => setOpen(true)} disableRipple={open}>
-            <CardContent className="projectCards">
-                <CardMedia
-                    component="img"
-                    image={project.image}
-                    alt="Project Image"
-                />
-                <Box>
-                    <Typography variant="h5">{project.name}</Typography>
-                    <div>
-                        {open && project.expandedDesc !== undefined ? project.expandedDesc : <Typography variant="body2" color="text.secondary">{project.description}</Typography>}
-                    </div>
-                </Box>
-            </CardContent>
-        </CardActionArea>
-    </Card>;
-
-    return (
-        <Grid item xs={1}>
-            <div className="NonModalWrapper">
-                {card}
-            </div>
-
-            <Modal
-                open={open}
-                onClose={() => setOpen(false)}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description" >
-                <Container className="ProjectModalContainer">
-                    <Box className="ProjectModalBox">
-                        {card}
-                    </Box>
-                </Container>
-            </Modal>
-        </Grid>
-    );
-}
-
+import Project from "../components/Project";
 
 const Projects: React.FC = () => {
     return (
