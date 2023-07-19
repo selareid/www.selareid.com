1110883cee3157289ed77e8ad218345e53cdac4b
Daniel <daniel@selareid.com>
2023-07-19 19:00:16 +1200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ function App() {
                 <header className="App-header">
                     <ButtonGroup variant="text" color="secondary" >
                         <Button className="navButton" component={Link} to={"/"}>home / about me</Button>
-                        /* <Button className="navButton" component={Link} to={"/projects"}>Projects</Button> */
                     </ButtonGroup>
                 </header>
 
