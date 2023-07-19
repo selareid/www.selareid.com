57e4c7c875c4ed7b4d1c9de5b107395d6b5dfe8c
Daniel <daniel@selareid.com>
2023-07-19 18:54:40 +1200

    Removed in-progress projects page from nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
                 <header className="App-header">
                     <ButtonGroup variant="text" color="secondary" >
                         <Button className="navButton" component={Link} to={"/"}>home / about me</Button>
-                        <Button className="navButton" component={Link} to={"/projects"}>Projects</Button>
+                        /* <Button className="navButton" component={Link} to={"/projects"}>Projects</Button> */
                     </ButtonGroup>
                 </header>
 
